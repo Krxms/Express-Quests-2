b99fa655216df6ca9b6979e6c2187bd27bb62684
Antoine <antoine.bruneau@protonmail.com>
2023-02-08 16:38:27 +0100

    Express quests challenge 04bis&5

--- app.js
+++ app.js
@@ -15,17 +15,21 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 const userHandlers = require("./userHandlers");
+const { validateMovie, validateUser } = require("./validators.js");
 
-app.get("/api/movies", movieHandlers.getMovies);
-app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+app.get("/api/movies", movieHandlers.getMovies);
+app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users", userHandlers.postUsers);
+app.post("/api/users", validateUser, userHandlers.postUsers);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
-app.put("/api/users/:id", userHandlers.updateUsers);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, userHandlers.updateUsers);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+
+app.delete("/api/users/:id", userHandlers.deleteUsers);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 app.listen(port, (err) => {
   if (err) {
